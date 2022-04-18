bb519fab9f919eab0b522eaf30eb0607b0d2e100
Guangzheng Wu <guangzhengwu@microsoft.com>
2022-04-18 15:29:33 +0800

    tmp changes

--- form.py
+++ form.py
@@ -21,13 +21,13 @@ def read_csv(fileName):
         except:
             return default
 
-    df['手机号'] = df['手机号'].apply(lambda x: tryGetValue(x, lambda y: str(int(y)), None))
     df['楼栋号'] = df['楼栋号'].apply(lambda x: tryGetValue(x, lambda y: int(y), 0))
-    df['门牌号'] = df['门牌号'].apply(lambda x: tryGetValue(x, lambda y: int(y), None))
+    df['门牌号'] = df['门牌号'].apply(lambda x: tryGetValue(x, lambda y: int(y), 0))
     df['订购数量'] = df['套餐数量'].apply(lambda x: tryGetValue(x, lambda y: int(y), 0))
     
     for i, ask in df.iterrows():
-        if ask['楼栋号'] == ask['门牌号']:
+        if ask['楼栋号'] == ask['门牌号'] or ask['门牌号'] == 0 or (ask['楼栋号'] <= 36 and ask['楼栋号'] >= 7):
+            df.loc[i, '门牌号'] = df.loc[i, '楼栋号']
             df.loc[i, '楼栋号'] = 500
 
     groups = df.groupby('楼栋号')
@@ -47,24 +47,21 @@ def generate_latex(groups, folder, title):
     doc.append(NoEscape(r'\renewcommand\arraystretch{1.2}'))
 
     doc.packages.add(Package('ctex'))
-    doc.preamble.append(Command('title', title + ' 2022/4/15'))
-    doc.append(NoEscape(r'\maketitle'))
 
     c = 0
+    index = 0
 
     for name, group in groups:
+        index += 1
 
         buildingNum = (str(int(name)) + '号楼')
 
         if name == 500:
             buildingNum = '别墅区'
-        elif name == 5889:
+        elif name == 5889 or name == 5899:
             buildingNum = '商务楼'
         elif name == 9999:
             buildingNum = '居委会'
-
-        if name == 500 or name == 0:
-            print(group)
 
         with doc.create(Section(buildingNum + '， 共' + str(int(sum(group['订购数量']))) + '单', False)):
             with doc.create(Tabular('|c|c|c|c|c|c|c|')) as table:
@@ -76,28 +73,32 @@ def generate_latex(groups, folder, title):
                     asks.append(ask)
 
                 asks.sort(key=lambda ask: ask['门牌号'])
-
                 for ask in asks:
                     table.add_row((
                             #ask['需求概述'] if name != 500 else buildingNum + str(int(ask['门牌号'])) + '号', 
-                            buildingNum + str(int(ask['门牌号'])) + '号', 
+                            buildingNum + str(int(ask['门牌号'])) + '号：' + title, 
                             ask['业主姓名'], 
                             ask['订单金额'],
                             buildingNum,
-                            str(int(ask['门牌号'])),
+                            str(int(ask['门牌号'])) if ask['门牌号'] != 0 else str(int(ask['楼栋号'])),
                             ask['套餐数量'],
                             ''))
                     table.add_hline()
                     c += ask['订购数量']
             doc.append('\n')
-            doc.append('签名：\n')
+            doc.append('签名：')
+            if (index != len(groups)):
+                doc.append('\n')
 
-    doc.generate_pdf(filepath=os.path.join(folder, title), compiler='xelatex', clean_tex=False)
+    doc.generate_pdf(filepath=os.path.join(folder, '居委团购-' + title), compiler='xelatex', clean_tex=False)
     tex = doc.dumps()
-    print(c)
+    print(title, '-', c)
 
 def main():
     generate_latex()
 
-groups = read_csv('files\\0415蔬菜(1).csv')
-generate_latex(groups, 'outputfiles', '居委团购-蔬菜')
+#pairs = [('files\\0416萃阳楼肉品.csv', '居委团购-萃阳楼肉品'), ('files\\0416防疫.csv', '居委团购-防疫'), ('files\\0416水产.csv', '居委团购-水产'), ('files\\0416水果.csv', '居委团购-水果')]
+pairs = [('files\\0418小番茄.csv', '0418小番茄'), ('files\\0418蔬菜.csv', '0418蔬菜')]
+for p in pairs:
+    groups = read_csv(p[0])
+    generate_latex(groups, 'outputfiles', p[1])
