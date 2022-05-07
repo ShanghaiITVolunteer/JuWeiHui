10b87c4b0996838ecbcfc79ed942a829d6a51dc3
Wu Guangzheng <jackw1997@sjtu.edu.cn>
2022-05-07 17:38:02 +0800

    Add support for other platform

--- group_purchase/utils/pdf_gen.py
+++ group_purchase/utils/pdf_gen.py
@@ -3,8 +3,7 @@ import pdfkit
 # TODO: 写的有点草率，之后再斟酌下。。。
 def html_string_to_pdf(html, filename):
 
-    pdfkit.from_string(html, filename, configuration=pdfkit.configuration(
-        wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'), options={
+    pdfkit.from_string(html, filename, options={
         'encoding': 'utf-8',
         'user-style-sheet': 'group_purchase/assets/html_format.css',
         'footer-center': '第[page]页  共[topage]页'
