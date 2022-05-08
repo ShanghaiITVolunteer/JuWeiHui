95ed684f3a5163a01fcacf7741f3d6e499bf7fb7
Guangzheng Wu <guangzhengwu@microsoft.com>
2022-05-08 13:58:01 +0800

    update css configuration

--- group_purchase/utils/pdf_gen.py
+++ group_purchase/utils/pdf_gen.py
@@ -1,14 +1,22 @@
 import pdfkit
 import os
+import platform
 
 # TODO: 写的有点草率，之后再斟酌下。。。
 def html_string_to_pdf(html, filename):
     file = os.path.abspath(__file__)
-    src_directory = os.path.join(os.path.pardir, os.path.dirname(file))
+    src_directory = os.path.dirname(os.path.dirname(file))
     css_file = os.path.join(src_directory, "assets", "html_format.css")
 
-    pdfkit.from_string(html, filename, options={
-        'encoding': 'utf-8',
-        'user-style-sheet': css_file,
-        'footer-center': '第[page]页  共[topage]页'
-    })
+    config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe') \
+        if platform.system() == 'Windows' else None
+
+    pdfkit.from_string(
+        html,
+        filename,
+        configuration=config,
+        options={
+            'encoding': 'utf-8',
+            'footer-center': '第[page]页  共[topage]页'
+        },
+        css=[css_file])
